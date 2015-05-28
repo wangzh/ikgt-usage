a480b1ed512f7276cacd4566d85c099754c3f6b1
lpan <Lu.Pan@intel.com>
2015-05-28 15:16:32 -0700

    Removed non-source file

--- driver/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lpan/GIT/Gerrit/Tests/ikgt_src_pkg/example_usage/integrity/driver/built-in.o :=  rm -f /home/lpan/GIT/Gerrit/Tests/ikgt_src_pkg/example_usage/integrity/driver/built-in.o; ar rcsD /home/lpan/GIT/Gerrit/Tests/ikgt_src_pkg/example_usage/integrity/driver/built-in.o
